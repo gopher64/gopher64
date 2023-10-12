1f6dda41772e44a53d5b29336637a70410ab2a03
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-10-12 08:15:52 -0600

    read and write

--- src/device/mempak.rs
+++ src/device/mempak.rs
@@ -1,7 +1,26 @@
 use crate::device;
+use crate::ui;
+
+pub const MEMPAK_SIZE: usize = 0x8000;
 
 pub fn read(device: &mut device::Device, channel: usize, address: u16, data: usize, size: usize) {
-    if address < 0x8000 {}
+    if (address as usize) < MEMPAK_SIZE {
+        let offset = (channel * MEMPAK_SIZE) + address as usize;
+        device.pif.ram[data..data + size]
+            .copy_from_slice(&device.ui.saves.mempak[offset..offset + size])
+    } else {
+        for i in 0..size {
+            device.pif.ram[data + i] = 0;
+        }
+    }
 }
 
-pub fn write(device: &mut device::Device, channel: usize, address: u16, data: usize, size: usize) {}
+pub fn write(device: &mut device::Device, channel: usize, address: u16, data: usize, size: usize) {
+    if (address as usize) < MEMPAK_SIZE {
+        let offset = (channel * MEMPAK_SIZE) + address as usize;
+        device.ui.saves.mempak[offset..offset + size]
+            .copy_from_slice(&device.pif.ram[data..data + size]);
+
+        ui::storage::write_save(&mut device.ui, ui::storage::SaveTypes::Mempak);
+    }
+}
