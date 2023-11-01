659f9e4e8f0b7c3399de3ca576359aea20199270
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-11-01 09:49:30 -0600

    more

--- src/ui/gui.rs
+++ src/ui/gui.rs
@@ -2,30 +2,30 @@ use crate::device;
 use eframe::egui;
 
 #[derive(Default)]
-pub struct GopherEguiApp {
-    picked_path: Option<String>,
-}
+pub struct GopherEguiApp {}
 
 impl eframe::App for GopherEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("Open ROM").clicked() {
-                if let Some(path) = rfd::FileDialog::new().pick_file() {
-                    self.picked_path = Some(path.display().to_string());
-                }
+                // Spawn dialog on main thread
+                let task = rfd::AsyncFileDialog::new().pick_file();
+
+                execute(async {
+                    let file = task.await;
+
+                    if let Some(file) = file {
+                        let mut device = device::Device::new();
+                        device::run_game(std::path::Path::new(file.path()), &mut device, false);
+                    }
+                });
             }
 
             ui.button("Configure Input Profile").clicked();
-
-            if let Some(picked_path) = &self.picked_path {
-                std::thread::scope(|s| {
-                    s.spawn(|| {
-                        let mut device = device::Device::new();
-                        device::run_game(std::path::Path::new(picked_path), &mut device, false);
-                    });
-                });
-                self.picked_path = None;
-            }
         });
     }
+}
+
+fn execute<F: std::future::Future<Output = ()> + Send + 'static>(f: F) {
+    std::thread::spawn(move || futures::executor::block_on(f));
 }
