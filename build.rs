8613e1644c046e3e05060ad179446c42a1f5e60f
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-01-02 10:58:37 +0100

    remove override

--- build.rs
+++ build.rs
@@ -161,7 +161,6 @@ fn main() {
         .file("gliden64/GLideN64/src/uCodes/ZSort.cpp")
         .file("gliden64/GLideN64/src/uCodes/ZSortBOSS.cpp")
         .file("gliden64/GLideN64/src/TxFilterStub.cpp")
-        .include("gliden64/overrides")
         .include("gliden64/GLideN64/src")
         .include("gliden64/GLideN64/src/inc")
         .include("gliden64/GLideN64/src/osal")
