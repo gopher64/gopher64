3b91350ed0dffe6258d472c73e2c36ed14baff79
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-01-13 07:40:47 +0100

    more

--- src/compat/aarch64.c
+++ src/compat/aarch64.c
@@ -3,3 +3,11 @@
 // Static wrappers
 
 __m128i _mm_srli_epi16__extern(__m128i __a, int __imm8) { return _mm_srli_epi16(__a, __imm8); }
+
+__m128i _mm_insert_epi8__extern(__m128i __a, int __i, const int __imm8) { return _mm_insert_epi8(__a, __i, __imm8); }
+
+int _mm_extract_epi8__extern(__m128i __a, const int __imm8) { return _mm_extract_epi8(__a, __imm8); }
+
+__m128i _mm_insert_epi16__extern(__m128i __a, int __i, const int __imm8) { return _mm_insert_epi16(__a, __i, __imm8); }
+
+int _mm_extract_epi16__extern(__m128i __a, const int __imm8) { return _mm_extract_epi16(__a, __imm8); }
--- src/compat/aarch64.rs
+++ src/compat/aarch64.rs
@@ -2,3 +2,31 @@ unsafe extern "C" {
     #[link_name = "_mm_srli_epi16__extern"]
     pub fn _mm_srli_epi16(__a: __m128i, __imm8: ::std::os::raw::c_int) -> __m128i;
 }
+
+unsafe extern "C" {
+    #[link_name = "_mm_insert_epi8__extern"]
+    pub fn _mm_insert_epi8(
+        __a: __m128i,
+        __i: ::std::os::raw::c_int,
+        __imm8: ::std::os::raw::c_int,
+    ) -> __m128i;
+}
+
+unsafe extern "C" {
+    #[link_name = "_mm_extract_epi8__extern"]
+    pub fn _mm_extract_epi8(__a: __m128i, __imm8: ::std::os::raw::c_int) -> i32;
+}
+
+unsafe extern "C" {
+    #[link_name = "_mm_insert_epi16__extern"]
+    pub fn _mm_insert_epi16(
+        __a: __m128i,
+        __i: ::std::os::raw::c_int,
+        __imm8: ::std::os::raw::c_int,
+    ) -> __m128i;
+}
+
+unsafe extern "C" {
+    #[link_name = "_mm_extract_epi16__extern"]
+    pub fn _mm_extract_epi16(__a: __m128i, __imm8: ::std::os::raw::c_int) -> i32;
+}
