413786ed1dda8761993c52cda3d6bee226db4e03
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-01-23 13:53:23 +0100

    var

--- src/device.rs
+++ src/device.rs
@@ -404,6 +404,7 @@ impl Device {
             },
             ri: ri::Ri {
                 regs: [0; ri::RI_REGS_COUNT as usize],
+                ram_init: false,
             },
             vi: vi::Vi {
                 regs: [0; vi::VI_REGS_COUNT as usize],
--- src/device/ri.rs
+++ src/device/ri.rs
@@ -12,6 +12,7 @@ pub const RI_REGS_COUNT: u32 = 8;
 
 pub struct Ri {
     pub regs: [u32; RI_REGS_COUNT as usize],
+    pub ram_init: bool,
 }
 
 pub fn read_regs(
@@ -19,8 +20,9 @@ pub fn read_regs(
     address: u64,
     _access_size: device::memory::AccessSize,
 ) -> u32 {
-    if ((address & 0xFFFF) >> 2) as u32 == RI_SELECT_REG {
+    if ((address & 0xFFFF) >> 2) as u32 == RI_SELECT_REG && !device.ri.ram_init {
         device::cop0::add_cycles(device, device.cpu.clock_rate / 2); // hack, simulate RDRAM initialization
+        device.ri.ram_init = true;
     }
     device.ri.regs[((address & 0xFFFF) >> 2) as usize]
 }
