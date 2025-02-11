2d36d296f45cd73719e2f5d0c40398346ca711d3
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-02-11 12:46:23 +0100

    arm

--- src/device.rs
+++ src/device.rs
@@ -4,7 +4,6 @@
 #[cfg(target_arch = "aarch64")]
 #[derive(Copy, Clone)]
 #[allow(dead_code)]
-#[repr(C)]
 pub struct __m128i(std::arch::aarch64::int64x2_t);
 #[cfg(target_arch = "aarch64")]
 include!(concat!(env!("OUT_DIR"), "/simd_bindings.rs"));
