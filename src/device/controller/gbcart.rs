54217ec56eea9262498b0694cfcd31e9a14fce49
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-02-23 13:10:01 +0100

    more

--- src/device/controller/gbcart.rs
+++ src/device/controller/gbcart.rs
@@ -1,5 +1,17 @@
 use crate::device;
 
+#[derive(Default, serde::Serialize, serde::Deserialize)]
+pub struct GbCart {
+    pub enabled: bool,
+    #[serde(skip)]
+    pub rom: Vec<u8>,
+    pub ram: Vec<u8>,
+    pub cart_type: device::controller::gbcart::CartType,
+    pub ram_enabled: bool,
+    pub ram_bank: u16,
+    pub rom_bank: u16,
+}
+
 #[derive(Default, Copy, Clone, serde::Serialize, serde::Deserialize)]
 pub enum CartType {
     #[default]
@@ -11,36 +23,36 @@ pub enum CartType {
 
 fn write_mbc3(
     pif_ram: &mut [u8],
-    pak: &mut device::controller::transferpak::TransferPak,
+    cart: &mut device::controller::gbcart::GbCart,
     address: u16,
     data: usize,
     size: usize,
 ) {
     let value = pif_ram[data + size - 1];
     if address < 0x2000 {
-        pak.ram_enabled = value & 0xf == 0xa;
+        cart.ram_enabled = value & 0xf == 0xa;
     } else if address < 0x4000 {
         let bank = value & 0x7f;
         if bank == 0 {
-            pak.bank = 1;
+            cart.rom_bank = 1;
         } else {
-            pak.bank = bank as u16;
+            cart.rom_bank = bank as u16;
         }
     } else if address < 0x6000 {
-        pak.ram_bank = value as u16;
+        cart.ram_bank = value as u16;
     } else if address < 0x8000 {
         println!("MBC3 RTC latch")
     } else if (0xa000..0xc000).contains(&address) {
-        if !pak.ram_enabled {
+        if !cart.ram_enabled {
             return;
         }
-        if pak.ram_bank < 0x8 {
-            let banked_address = (pak.ram_bank << 13) | (address & 0x1FFF);
+        if cart.ram_bank < 0x8 {
+            let banked_address = (cart.ram_bank << 13) | (address & 0x1FFF);
             for i in 0..size {
-                pak.ram[banked_address as usize + i] = pif_ram[data + i];
+                cart.ram[banked_address as usize + i] = pif_ram[data + i];
             }
         } else {
-            panic!("Unsupported ram bank {:x}", pak.ram_bank);
+            panic!("Unsupported ram bank {:x}", cart.ram_bank);
         }
     } else {
         panic!("Unsupported write address {:x}", address);
@@ -49,34 +61,34 @@ fn write_mbc3(
 
 fn read_mbc3(
     pif_ram: &mut [u8],
-    pak: &mut device::controller::transferpak::TransferPak,
+    cart: &mut device::controller::gbcart::GbCart,
     address: u16,
     data: usize,
     size: usize,
 ) {
     if address < 0x4000 {
         for i in 0..size {
-            pif_ram[data + i] = pak.rom[address as usize + i];
+            pif_ram[data + i] = cart.rom[address as usize + i];
         }
     } else if address < 0x8000 {
-        let banked_address = (pak.bank << 14) | (address & 0x3FFF);
+        let banked_address = (cart.rom_bank << 14) | (address & 0x3FFF);
         for i in 0..size {
-            pif_ram[data + i] = pak.rom[banked_address as usize + i];
+            pif_ram[data + i] = cart.rom[banked_address as usize + i];
         }
     } else if address < 0xc000 {
-        if !pak.ram_enabled {
+        if !cart.ram_enabled {
             for i in 0..size {
                 pif_ram[data + i] = 0xff;
             }
             return;
         }
-        if pak.ram_bank < 0x8 {
-            let banked_address = (pak.ram_bank << 13) | (address & 0x1FFF);
+        if cart.ram_bank < 0x8 {
+            let banked_address = (cart.ram_bank << 13) | (address & 0x1FFF);
             for i in 0..size {
-                pif_ram[data + i] = pak.ram[banked_address as usize + i];
+                pif_ram[data + i] = cart.ram[banked_address as usize + i];
             }
         } else {
-            panic!("Unsupported ram bank {:x}", pak.ram_bank);
+            panic!("Unsupported ram bank {:x}", cart.ram_bank);
         }
     } else {
         panic!("Unsupported read address {:x}", address);
@@ -85,28 +97,28 @@ fn read_mbc3(
 
 fn write_mbc5(
     pif_ram: &mut [u8],
-    pak: &mut device::controller::transferpak::TransferPak,
+    cart: &mut device::controller::gbcart::GbCart,
     address: u16,
     data: usize,
     size: usize,
 ) {
     let value = pif_ram[data + size - 1];
     if address < 0x2000 {
-        pak.ram_enabled = value & 0xf == 0xa;
+        cart.ram_enabled = value & 0xf == 0xa;
     } else if address < 0x4000 {
         println!("Unknown MBC5 write address {:x}", address);
     } else if address < 0x6000 {
-        pak.ram_bank = (value & 0xf) as u16;
+        cart.ram_bank = (value & 0xf) as u16;
     } else if address < 0xa000 {
         println!("Unknown MBC5 write address {:x}", address);
     } else if (0xa000..0xc000).contains(&address) {
-        if !pak.ram_enabled {
+        if !cart.ram_enabled {
             return;
         }
 
-        let banked_address = (pak.ram_bank << 13) | (address & 0x1FFF);
+        let banked_address = (cart.ram_bank << 13) | (address & 0x1FFF);
         for i in 0..size {
-            pak.ram[banked_address as usize + i] = pif_ram[data + i];
+            cart.ram[banked_address as usize + i] = pif_ram[data + i];
         }
     } else {
         panic!("Unsupported write address {:x}", address);
@@ -115,31 +127,31 @@ fn write_mbc5(
 
 fn read_mbc5(
     pif_ram: &mut [u8],
-    pak: &mut device::controller::transferpak::TransferPak,
+    cart: &mut device::controller::gbcart::GbCart,
     address: u16,
     data: usize,
     size: usize,
 ) {
     if address < 0x4000 {
         for i in 0..size {
-            pif_ram[data + i] = pak.rom[address as usize + i];
+            pif_ram[data + i] = cart.rom[address as usize + i];
         }
     } else if address < 0x8000 {
-        let banked_address = (pak.bank << 14) | (address & 0x3FFF);
+        let banked_address = (cart.rom_bank << 14) | (address & 0x3FFF);
         for i in 0..size {
-            pif_ram[data + i] = pak.rom[banked_address as usize + i];
+            pif_ram[data + i] = cart.rom[banked_address as usize + i];
         }
     } else if (0xa000..0xc000).contains(&address) {
-        if !pak.ram_enabled {
+        if !cart.ram_enabled {
             for i in 0..size {
                 pif_ram[data + i] = 0xff;
             }
             return;
         }
 
-        let banked_address = (pak.ram_bank << 13) | (address & 0x1FFF);
+        let banked_address = (cart.ram_bank << 13) | (address & 0x1FFF);
         for i in 0..size {
-            pif_ram[data + i] = pak.ram[banked_address as usize + i];
+            pif_ram[data + i] = cart.ram[banked_address as usize + i];
         }
     } else {
         panic!("Unsupported read address {:x}", address);
@@ -148,39 +160,39 @@ fn read_mbc5(
 
 pub fn read(
     pif_ram: &mut [u8],
-    pak: &mut device::controller::transferpak::TransferPak,
+    cart: &mut device::controller::gbcart::GbCart,
     address: u16,
     data: usize,
     size: usize,
 ) {
-    if !pak.cart_enabled {
+    if !cart.enabled {
         for i in 0..size {
             pif_ram[data + i] = 0x00;
         }
         return;
     }
-    match pak.cart_type {
-        CartType::MBC3RamBatt => read_mbc3(pif_ram, pak, address, data, size),
-        CartType::MBC3RamBattRtc => read_mbc3(pif_ram, pak, address, data, size),
-        CartType::MBC5RamBatt => read_mbc5(pif_ram, pak, address, data, size),
+    match cart.cart_type {
+        CartType::MBC3RamBatt => read_mbc3(pif_ram, cart, address, data, size),
+        CartType::MBC3RamBattRtc => read_mbc3(pif_ram, cart, address, data, size),
+        CartType::MBC5RamBatt => read_mbc5(pif_ram, cart, address, data, size),
         _ => panic!("Unsupported cart type"),
     }
 }
 
 pub fn write(
     pif_ram: &mut [u8],
-    pak: &mut device::controller::transferpak::TransferPak,
+    cart: &mut device::controller::gbcart::GbCart,
     address: u16,
     data: usize,
     size: usize,
 ) {
-    if !pak.cart_enabled {
+    if !cart.enabled {
         return;
     }
-    match pak.cart_type {
-        CartType::MBC3RamBatt => write_mbc3(pif_ram, pak, address, data, size),
-        CartType::MBC3RamBattRtc => write_mbc3(pif_ram, pak, address, data, size),
-        CartType::MBC5RamBatt => write_mbc5(pif_ram, pak, address, data, size),
+    match cart.cart_type {
+        CartType::MBC3RamBatt => write_mbc3(pif_ram, cart, address, data, size),
+        CartType::MBC3RamBattRtc => write_mbc3(pif_ram, cart, address, data, size),
+        CartType::MBC5RamBatt => write_mbc5(pif_ram, cart, address, data, size),
         _ => panic!("Unsupported cart type"),
     }
 }
