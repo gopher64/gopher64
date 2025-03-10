25233ed4911e36827c917ddacbcfd89989a7066b
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-03-10 17:08:49 +0100

    update shaders

--- data/shader/output.sh
+++ data/shader/output.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-glslc -O crt-aperture.frag -o frag.spv --target-env=vulkan1.1 -mfmt=c
+glslc -O crt-aperture.frag -o crt_frag.spv --target-env=vulkan1.1 -mfmt=c
+glslc -O main.frag -o main_frag.spv --target-env=vulkan1.1 -mfmt=c
+glslc -O main.vert -o main_vert.spv --target-env=vulkan1.1 -mfmt=c
