b2f4bd70c2dd8ae0e9ec6017234884d09f9bdf4d
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-06-16 23:23:02 +0200

    more

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("cargo::rerun-if-changed=parallel-rdp");
     println!("cargo::rerun-if-changed=src/compat");
 
-    let slint_config = slint_build::CompilerConfiguration::new().with_style("fluent".into());
+    let slint_config = slint_build::CompilerConfiguration::new().with_style("cosmic".into());
     slint_build::compile_with_config("src/ui/gui/appwindow.slint", slint_config).unwrap();
 
     let mut simd_build = cc::Build::new();
