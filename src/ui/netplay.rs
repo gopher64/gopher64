2c716b38ce40ee4b1d7b6a6c717243ae2c3c4825
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-06-20 13:19:39 +0200

    more

--- src/ui/netplay.rs
+++ src/ui/netplay.rs
@@ -2,7 +2,43 @@ use slint::{ComponentHandle, Model};
 
 use crate::ui::gui::{NetplayCreate, NetplayJoin};
 use futures::{SinkExt, StreamExt};
+use sha2::{Digest, Sha256};
+use tokio_tungstenite::tungstenite::Bytes;
 use tokio_tungstenite::tungstenite::protocol::Message;
+
+const NETPLAY_VERSION: i32 = 17;
+const EMU_NAME: &str = "gopher64";
+
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Default, Clone)]
+pub struct NetplayRoom {
+    room_name: Option<String>,
+    password: Option<String>,
+    protected: Option<bool>,
+    #[serde(rename = "MD5")]
+    md5: Option<String>,
+    game_name: Option<String>,
+    pub port: Option<i32>,
+    features: Option<std::collections::HashMap<String, String>>,
+    buffer_target: Option<i32>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct NetplayMessage {
+    #[serde(rename = "type")]
+    message_type: String,
+    player_name: Option<String>,
+    client_sha: Option<String>,
+    netplay_version: Option<i32>,
+    emulator: Option<String>,
+    room: Option<NetplayRoom>,
+    accept: Option<i32>,
+    message: Option<String>,
+    auth: Option<String>,
+    player_names: Option<[String; 4]>,
+    #[serde(rename = "authTime")]
+    auth_time: Option<String>,
+    rooms: Option<Vec<NetplayRoom>>,
+}
 
 trait NetplayPages {
     fn set_server_names(&self, names: slint::ModelRc<slint::SharedString>);
@@ -138,6 +174,51 @@ pub fn setup_join_window(join_window: &NetplayJoin) {
     let weak2 = join_window.as_weak();
     join_window.on_get_ping(move |server_url| {
         update_ping(weak2.clone(), server_url.to_string());
+    });
+
+    join_window.on_refresh_session(move |server_url| {
+        tokio::spawn(async move {
+            if let Ok(Ok((mut socket, _response))) = tokio::time::timeout(
+                std::time::Duration::from_secs(1),
+                tokio_tungstenite::connect_async(server_url.to_string()),
+            )
+            .await
+            {
+                let now_utc = chrono::Utc::now().timestamp_millis().to_string();
+                let hasher = Sha256::new().chain_update(&now_utc).chain_update(EMU_NAME);
+                let request_rooms = NetplayMessage {
+                    message_type: "request_get_rooms".to_string(),
+                    player_name: None,
+                    client_sha: None,
+                    netplay_version: Some(NETPLAY_VERSION),
+                    player_names: None,
+                    emulator: Some(EMU_NAME.to_string()),
+                    accept: None,
+                    rooms: None,
+                    message: None,
+                    auth_time: Some(now_utc),
+                    auth: Some(format!("{:x}", hasher.finalize())),
+                    room: None,
+                };
+
+                socket
+                    .send(Message::Binary(Bytes::from(
+                        serde_json::to_vec(&request_rooms).unwrap(),
+                    )))
+                    .await
+                    .unwrap();
+
+                if let Some(Ok(response)) = socket.next().await {
+                    let message: NetplayMessage =
+                        serde_json::from_slice(&response.into_data()).unwrap();
+                    if message.accept.unwrap() == 0 {
+                        //populate the rooms
+                    } else {
+                        //pop up error
+                    }
+                }
+            }
+        });
     });
 
     join_window.show().unwrap();
