fc2ab067683decbdba29052fa3ac8cfc64f39274
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-06-21 19:38:23 +0200

    more

--- src/ui/netplay.rs
+++ src/ui/netplay.rs
@@ -212,10 +212,10 @@ fn manage_websocket(
 
 fn update_sessions(
     server_url: String,
-    netplay_write_sender: tokio::sync::broadcast::Sender<Option<NetplayMessage>>,
-    netplay_read_sender: tokio::sync::broadcast::Sender<NetplayMessage>,
-    netplay_write_receiver: tokio::sync::broadcast::Receiver<Option<NetplayMessage>>,
-    netplay_read_receiver: tokio::sync::broadcast::Receiver<NetplayMessage>,
+    netplay_write_sender: &tokio::sync::broadcast::Sender<Option<NetplayMessage>>,
+    netplay_read_sender: &tokio::sync::broadcast::Sender<NetplayMessage>,
+    netplay_write_receiver: &tokio::sync::broadcast::Receiver<Option<NetplayMessage>>,
+    netplay_read_receiver: &tokio::sync::broadcast::Receiver<NetplayMessage>,
     weak: slint::Weak<NetplayJoin>,
 ) {
     netplay_write_sender.send(None).unwrap(); // close current websocket if any
@@ -314,21 +314,13 @@ pub fn setup_join_window(join_window: &NetplayJoin) {
     let weak = join_window.as_weak();
     populate_server_names(weak);
     let weak2 = join_window.as_weak();
-    let netplay_write_sender2 = netplay_write_sender.clone();
-    let netplay_read_sender2 = netplay_read_sender.clone();
-    let netplay_write_receiver2 = netplay_write_receiver.resubscribe();
-    let netplay_read_receiver2 = netplay_read_receiver.resubscribe();
     join_window.on_get_ping(move |server_url| {
         update_ping(weak2.clone(), server_url.to_string());
-
-        update_sessions(
-            server_url.to_string(),
-            netplay_write_sender2.clone(),
-            netplay_read_sender2.clone(),
-            netplay_write_receiver2.resubscribe(),
-            netplay_read_receiver2.resubscribe(),
-            weak2.clone(),
-        );
+        weak2
+            .upgrade_in_event_loop(move |handle| {
+                handle.refresh_sessions(server_url);
+            })
+            .unwrap();
     });
 
     let netplay_write_sender_closed = netplay_write_sender.clone();
@@ -338,17 +330,13 @@ pub fn setup_join_window(join_window: &NetplayJoin) {
     });
 
     let weak3 = join_window.as_weak();
-    let netplay_write_sender3 = netplay_write_sender.clone();
-    let netplay_read_sender3 = netplay_read_sender.clone();
-    let netplay_write_receiver3 = netplay_write_receiver.resubscribe();
-    let netplay_read_receiver3 = netplay_read_receiver.resubscribe();
     join_window.on_refresh_session(move |server_url| {
         update_sessions(
             server_url.to_string(),
-            netplay_write_sender3.clone(),
-            netplay_read_sender3.clone(),
-            netplay_write_receiver3.resubscribe(),
-            netplay_read_receiver3.resubscribe(),
+            &netplay_write_sender,
+            &netplay_read_sender,
+            &netplay_write_receiver,
+            &netplay_read_receiver,
             weak3.clone(),
         );
     });
