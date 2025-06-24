c7e130d445162fbe3b2306e2431b4c8ca4e6ae61
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-06-24 21:58:20 +0200

    more

--- src/ui/netplay.rs
+++ src/ui/netplay.rs
@@ -619,7 +619,11 @@ fn setup_wait_window(
                     netplay_write_sender.send(Some(request_players)).unwrap();
 
                     weak.upgrade_in_event_loop(move |handle| {
-                        handle.set_motd(response.message.unwrap().into());
+                        let re = regex::Regex::new(r"<[^>]*>").unwrap();
+                        let motd = re
+                            .replace_all(response.message.unwrap().as_str(), "")
+                            .into_owned();
+                        handle.set_motd(motd.into());
                     })
                     .unwrap();
                 }
