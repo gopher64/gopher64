1d1e3829e442fc288979ba1036b9c9a7f0d6ede6
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-07-14 10:42:29 +0200

    Update icon

---
+++ data/icon/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+magick gopher64.png -resize 512x512 gopher64_512x512.png
+magick gopher64.png -resize 256x256 gopher64_256x256.png
+magick gopher64.png -resize 128x128 gopher64_128x128.png
+magick gopher64.png -resize 256x256 icon.ico
