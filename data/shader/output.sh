6851cedbff2498f47fd1e7c234f056a6a67c8788
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-08-04 18:52:31 +0200

    more

--- data/shader/output.sh
+++ data/shader/output.sh
@@ -3,5 +3,3 @@
 glslc -O crt-aperture.frag -o crt_frag.spv --target-env=vulkan1.1 -mfmt=c
 glslc -O main.frag -o main_frag.spv --target-env=vulkan1.1 -mfmt=c
 glslc -O main.vert -o main_vert.spv --target-env=vulkan1.1 -mfmt=c
-glslc -O text.frag -o text_frag.spv --target-env=vulkan1.1 -mfmt=c
-glslc -O text.vert -o text_vert.spv --target-env=vulkan1.1 -mfmt=c
