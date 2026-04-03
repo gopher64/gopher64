de6e1fdb58cf764427e630ad2b8a58eca72db6e6
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-04-03 16:05:41 +0200

    write savestate in thread

--- src/savestates.rs
+++ src/savestates.rs
@@ -81,12 +81,11 @@ pub fn create_savestate(device: &device::Device) {
         ),
         (&rdp_state, "rdp_state"),
     ];
+    let save_path = device.ui.storage.paths.savestate_file_path.clone();
     let compressed_file = ui::storage::compress_file(data);
-    std::fs::write(
-        device.ui.storage.paths.savestate_file_path.clone(),
-        compressed_file,
-    )
-    .unwrap();
+    tokio::spawn(async move {
+        tokio::fs::write(save_path, compressed_file).await.unwrap();
+    });
     ui::video::onscreen_message(
         &device.ui,
         &format!(
