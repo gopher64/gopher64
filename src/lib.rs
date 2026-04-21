b403ee6ee05c7ab67fadd1a6e6ef8be9d1fa9367
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-04-21 16:33:39 +0200

    more

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod savestates;
 mod ui;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn gopher64_run_game(c_rom_contents: *const u8, rom_size: usize) {
+pub extern "C" fn gopher64_run_game(rom_contents: *const u8, rom_size: usize) {
     let dirs = ui::get_dirs();
 
     std::fs::create_dir_all(dirs.config_dir).unwrap();
@@ -15,13 +15,12 @@ pub extern "C" fn gopher64_run_game(c_rom_contents: *const u8, rom_size: usize) 
     std::fs::create_dir_all(dirs.data_dir.join("saves")).unwrap();
     std::fs::create_dir_all(dirs.data_dir.join("states")).unwrap();
 
-    let rom_contents = unsafe { std::slice::from_raw_parts(c_rom_contents, rom_size) };
     let mut device = device::Device::new();
 
     device.ui.video.fullscreen = device.ui.config.video.fullscreen;
     device::run_game(
         &mut device,
-        &rom_contents,
+        &unsafe { std::slice::from_raw_parts(rom_contents, rom_size) },
         ui::GameSettings {
             overclock: false,
             disable_expansion_pak: false,
