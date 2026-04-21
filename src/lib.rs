58069c3973c118c7dee7330b5f2d5140eb6ca9eb
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-04-21 16:29:13 +0200

    more

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,17 @@ mod ui;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn run_game(c_rom_contents: *const u8, rom_size: usize) {
+    let dirs = ui::get_dirs();
+
+    std::fs::create_dir_all(dirs.config_dir).unwrap();
+    std::fs::create_dir_all(dirs.cache_dir).unwrap();
+    std::fs::create_dir_all(dirs.data_dir.join("saves")).unwrap();
+    std::fs::create_dir_all(dirs.data_dir.join("states")).unwrap();
+
     let rom_contents = unsafe { std::slice::from_raw_parts(c_rom_contents, rom_size) };
     let mut device = device::Device::new();
+
+    device.ui.video.fullscreen = device.ui.config.video.fullscreen;
     device::run_game(
         &mut device,
         &rom_contents,
