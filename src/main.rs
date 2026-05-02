6ba5c8a1ce95823a53ef5d6006b3a41a680c1110
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-02 16:08:06 +0200

    avoid crash on missing GB files

--- src/main.rs
+++ src/main.rs
@@ -180,10 +180,12 @@ async fn main() -> std::io::Result<()> {
                     && !device.ui.config.input.gb_rom_path[i].is_empty()
                     && !device.ui.config.input.gb_ram_path[i].is_empty()
                 {
-                    device.transferpaks[i].cart.rom =
-                        std::fs::read(&device.ui.config.input.gb_rom_path[i]).unwrap();
-                    device.transferpaks[i].cart.ram =
-                        std::fs::read(&device.ui.config.input.gb_ram_path[i]).unwrap();
+                    if let Ok(rom) = std::fs::read(&device.ui.config.input.gb_rom_path[i])
+                        && let Ok(ram) = std::fs::read(&device.ui.config.input.gb_ram_path[i])
+                    {
+                        device.transferpaks[i].cart.rom = rom;
+                        device.transferpaks[i].cart.ram = ram;
+                    }
                 }
             }
 
@@ -234,13 +236,13 @@ async fn main() -> std::io::Result<()> {
             for i in 0..4 {
                 if device.ui.config.input.transfer_pak[i]
                     && !device.ui.config.input.gb_ram_path[i].is_empty()
+                    && !device.transferpaks[i].cart.ram.is_empty()
                 {
-                    tokio::fs::write(
+                    let _ = tokio::fs::write(
                         &device.ui.config.input.gb_ram_path[i],
                         &device.transferpaks[i].cart.ram,
                     )
-                    .await
-                    .unwrap();
+                    .await;
                 }
             }
         }
