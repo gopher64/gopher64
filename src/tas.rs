8c9a35dfcffd996b6ffde38959dd9a16f8f2bf2a
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-12 22:41:28 +0200

    more

--- src/tas.rs
+++ src/tas.rs
@@ -1,3 +1,7 @@
+use std::io::Read;
+
+use crate::ui;
+
 pub fn load_tas(tas_file: String) -> Vec<u32> {
     let path = std::path::Path::new(&tas_file);
     if path
@@ -6,10 +10,87 @@ pub fn load_tas(tas_file: String) -> Vec<u32> {
         .eq_ignore_ascii_case("m64")
     {
         load_m64(tas_file)
+    } else if path
+        .extension()
+        .unwrap_or_default()
+        .eq_ignore_ascii_case("bk2")
+    {
+        let mut input_log = Vec::new();
+        if let Ok(zip_file) = std::fs::File::open(tas_file)
+            && let Ok(mut archive) = zip::ZipArchive::new(zip_file)
+            && let Ok(mut file) = archive.by_name("Input Log.txt")
+        {
+            file.read_to_end(&mut input_log)
+                .expect("could not read zip file");
+        }
+        if !input_log.is_empty() {
+            load_bk2(String::from_utf8(input_log).unwrap())
+        } else {
+            eprintln!("could not read TAS file");
+            Vec::new()
+        }
     } else {
         eprintln!("could not load TAS file");
         Vec::new()
     }
+}
+
+fn load_bk2(input_log: String) -> Vec<u32> {
+    let mut tas = Vec::new();
+    for line in input_log.lines() {
+        if line.starts_with("|") {
+            let mut keys: u32 = 0;
+            let data = line.split('|').nth(2).unwrap();
+            let x_axis = data
+                .split(',')
+                .nth(0)
+                .unwrap()
+                .trim_start()
+                .parse::<i8>()
+                .unwrap();
+            let y_axis = data
+                .split(',')
+                .nth(1)
+                .unwrap()
+                .trim_start()
+                .parse::<i8>()
+                .unwrap();
+            let buttons = data.split(',').nth(2).unwrap();
+            let u_dpad = buttons.chars().nth(4).unwrap() != '.';
+            let d_dpad = buttons.chars().nth(5).unwrap() != '.';
+            let l_dpad = buttons.chars().nth(6).unwrap() != '.';
+            let r_dpad = buttons.chars().nth(7).unwrap() != '.';
+            let start_button = buttons.chars().nth(8).unwrap() != '.';
+            let z_trig = buttons.chars().nth(9).unwrap() != '.';
+            let b_button = buttons.chars().nth(10).unwrap() != '.';
+            let a_button = buttons.chars().nth(11).unwrap() != '.';
+            let u_cbutton = buttons.chars().nth(12).unwrap() != '.';
+            let d_cbutton = buttons.chars().nth(13).unwrap() != '.';
+            let l_cbutton = buttons.chars().nth(14).unwrap() != '.';
+            let r_cbutton = buttons.chars().nth(15).unwrap() != '.';
+            let l_trig = buttons.chars().nth(16).unwrap() != '.';
+            let r_trig = buttons.chars().nth(17).unwrap() != '.';
+            keys |= (u_dpad as u32) << ui::input::U_DPAD;
+            keys |= (d_dpad as u32) << ui::input::D_DPAD;
+            keys |= (l_dpad as u32) << ui::input::L_DPAD;
+            keys |= (r_dpad as u32) << ui::input::R_DPAD;
+            keys |= (start_button as u32) << ui::input::START_BUTTON;
+            keys |= (z_trig as u32) << ui::input::Z_TRIG;
+            keys |= (b_button as u32) << ui::input::B_BUTTON;
+            keys |= (a_button as u32) << ui::input::A_BUTTON;
+            keys |= (u_cbutton as u32) << ui::input::U_CBUTTON;
+            keys |= (d_cbutton as u32) << ui::input::D_CBUTTON;
+            keys |= (l_cbutton as u32) << ui::input::L_CBUTTON;
+            keys |= (r_cbutton as u32) << ui::input::R_CBUTTON;
+            keys |= (l_trig as u32) << ui::input::L_TRIG;
+            keys |= (r_trig as u32) << ui::input::R_TRIG;
+            keys |= (x_axis as u8 as u32) << ui::input::X_AXIS_SHIFT;
+            keys |= (y_axis as u8 as u32) << ui::input::Y_AXIS_SHIFT;
+            tas.push(keys);
+        }
+    }
+    println!("TAS file loaded successfully");
+    tas
 }
 
 fn load_m64(tas_file: String) -> Vec<u32> {
--- src/ui/input.rs
+++ src/ui/input.rs
@@ -2,20 +2,20 @@ use std::ops::Neg;
 
 use crate::ui;
 
-const R_DPAD: usize = 0;
-const L_DPAD: usize = 1;
-const D_DPAD: usize = 2;
-const U_DPAD: usize = 3;
-const START_BUTTON: usize = 4;
-const Z_TRIG: usize = 5;
-const B_BUTTON: usize = 6;
-const A_BUTTON: usize = 7;
-const R_CBUTTON: usize = 8;
-const L_CBUTTON: usize = 9;
-const D_CBUTTON: usize = 10;
-const U_CBUTTON: usize = 11;
-const R_TRIG: usize = 12;
-const L_TRIG: usize = 13;
+pub const R_DPAD: usize = 0;
+pub const L_DPAD: usize = 1;
+pub const D_DPAD: usize = 2;
+pub const U_DPAD: usize = 3;
+pub const START_BUTTON: usize = 4;
+pub const Z_TRIG: usize = 5;
+pub const B_BUTTON: usize = 6;
+pub const A_BUTTON: usize = 7;
+pub const R_CBUTTON: usize = 8;
+pub const L_CBUTTON: usize = 9;
+pub const D_CBUTTON: usize = 10;
+pub const U_CBUTTON: usize = 11;
+pub const R_TRIG: usize = 12;
+pub const L_TRIG: usize = 13;
 const AXIS_LEFT: usize = 14;
 const AXIS_RIGHT: usize = 15;
 const AXIS_UP: usize = 16;
@@ -23,8 +23,8 @@ const AXIS_DOWN: usize = 17;
 const HOTKEY: usize = 18;
 pub const PROFILE_SIZE: usize = 19;
 
-const X_AXIS_SHIFT: usize = 16;
-const Y_AXIS_SHIFT: usize = 24;
+pub const X_AXIS_SHIFT: usize = 16;
+pub const Y_AXIS_SHIFT: usize = 24;
 
 const MAX_AXIS_VALUE: f64 = 85.0;
 
