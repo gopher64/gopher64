50e06c8ec28a1042a0c5ee24fe3c792c5a0c6299
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-13 07:55:24 +0200

    more

--- src/tas.rs
+++ src/tas.rs
@@ -5,9 +5,17 @@ pub fn load_m64(tas_file: String) -> Vec<u32> {
         let num_controllers = m64_file[0x15];
         let start_type = u16::from_le_bytes(m64_file[0x1c..0x1e].try_into().unwrap());
 
-        if signature == 0x1a34364d && version == 3 && num_controllers == 1 && start_type == 2 {
+        let offset = if version == 3 {
+            0x400
+        } else if version == 2 || version == 1 {
+            0x200
+        } else {
+            0
+        };
+
+        if signature == 0x1a34364d && offset != 0 && num_controllers == 1 && start_type == 2 {
             println!("TAS file loaded successfully");
-            m64_file[0x400..]
+            m64_file[offset..]
                 .chunks_exact(4)
                 .map(|chunk| u32::from_le_bytes(chunk.try_into().unwrap()))
                 .collect()
