715e498f1b3af4620300d4aeb2c19156e951dceb
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-15 12:43:04 +0200

    more

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[tokio::main(worker_threads = 4)]
 async fn main() -> std::io::Result<()> {
-    gopher::run().await
+    gopher64::run().await
 }
