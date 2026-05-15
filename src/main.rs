fe21724fe46a44270cd94ecd89c45fb786a8278f
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-15 11:52:54 +0200

    more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![deny(warnings)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
