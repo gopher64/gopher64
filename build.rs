3be5cfe4a31cfba295c72a30cc760cc20c7c31c7
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-18 16:57:41 +0200

    more

--- build.rs
+++ build.rs
@@ -306,31 +306,4 @@ fn main() {
     {
         println!("cargo:rustc-cfg=ra_hardcore_enabled");
     }
-
-    if os == "android" {
-        std::fs::create_dir_all("data/android/java").unwrap();
-        copy_dir_all(
-            std::path::PathBuf::from(sdl3_src::SOURCE_DIR)
-                .join("android-project/app/src/main/java"),
-            "data/android/java",
-        )
-        .unwrap();
-    }
-}
-
-fn copy_dir_all(
-    src: impl AsRef<std::path::Path>,
-    dst: impl AsRef<std::path::Path>,
-) -> std::io::Result<()> {
-    std::fs::create_dir_all(&dst)?;
-    for entry in std::fs::read_dir(src)? {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        if ty.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        } else {
-            std::fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        }
-    }
-    Ok(())
 }
