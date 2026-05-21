09136b66d44ab0a56b84fd4657df57baf632be12
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-21 08:57:36 +0200

    more

--- android-project/app/build.gradle.kts
+++ android-project/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
