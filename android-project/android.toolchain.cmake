b865c88d99501f41ac118a5429d882931643f7f3
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-21 18:16:12 +0200

    Use Gradle (#931)
    * remove cargo-apk
    
    * add gradle
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * Revert "more"
    
    This reverts commit 19395aac139976c9fd1b330f56ab03ac78236dc8.
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more
    
    * more

---
+++ android-project/android.toolchain.cmake
@@ -0,0 +1,3 @@
+set(ANDROID_PLATFORM $ENV{ANDROID_PLATFORM})
+set(ANDROID_ABI $ENV{ANDROID_ABI})
+include("$ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake")
---
+++ android-project/app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "io.github.gopher64.gopher64"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    ndkVersion = "27.3.13750724"
+
+    defaultConfig {
+        applicationId = "io.github.gopher64.gopher64"
+        minSdk = 34
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.1.20"
+        ndk {
+            abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
+        }
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    packaging {
+        jniLibs {
+            excludes.add("lib/**/libsevenz_rust2*.so")
+        }
+    }
+}
+
+val ndkBuild = tasks.register<Exec>("ndkBuild") {
+    val isRelease = gradle.startParameter.taskNames.any { it.endsWith("Release", ignoreCase = true) }
+    workingDir = rootDir.parentFile
+    val toolchainPath = "$rootDir/android.toolchain.cmake"
+    environment("CMAKE_TOOLCHAIN_FILE", toolchainPath)
+
+    var minSdk = android.defaultConfig.minSdk
+    var ndkDir = androidComponents.sdkComponents.ndkDirectory.get().asFile.absolutePath
+    environment("ANDROID_NDK_HOME", "$ndkDir")
+    environment("LIBCLANG_PATH", "$ndkDir/toolchains/llvm/prebuilt/linux-x86_64/musl/lib")
+
+    val jniLibsFolder = layout.projectDirectory.dir("$rootDir/app/src/main/jniLibs")
+    project.delete(jniLibsFolder)
+
+    commandLine(
+        "cargo", "ndk",
+        "--link-libcxx-shared",
+        "-P", "$minSdk",
+        "-t", "arm64-v8a",
+        "-t", "x86_64",
+        "-o", "$rootDir/app/src/main/jniLibs",
+        "build", "--lib",
+        "--profile", if (isRelease) "release" else "dev",
+    )
+}
+
+tasks.named("preBuild") {
+    dependsOn(ndkBuild)
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+}
