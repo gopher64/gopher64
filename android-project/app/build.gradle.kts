8e76ac4799cca619d1116689c30c9dba4d25f2dd
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-21 14:36:16 +0200

    more

--- android-project/app/build.gradle.kts
+++ android-project/app/build.gradle.kts
@@ -43,6 +43,27 @@ android {
     }
 }
 
+val ndkBuild = tasks.register<Exec>("ndkBuild") {
+    val isRelease = gradle.startParameter.taskNames.any { it.contains("Release") }
+    workingDir = rootDir.parentFile
+    val toolchainPath = "$rootDir/android.toolchain.cmake"
+    environment("CMAKE_TOOLCHAIN_FILE", toolchainPath)
+    commandLine(
+        "cargo", "ndk",
+        "--link-libcxx-shared",
+        "-P", "35",
+        "-t", "arm64-v8a",
+        "-t", "x86_64",
+        "-o", "$rootDir/app/src/main/jniLibs",
+        "build", "--lib",
+        "--profile", if (isRelease) "release" else "dev",
+    )
+}
+
+tasks.named("preBuild") {
+    dependsOn(ndkBuild)
+}
+
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
