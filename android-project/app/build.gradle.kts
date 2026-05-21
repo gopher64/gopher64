6e011558a8b6d1f73b983f90c0e58f6b47cbc357
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-21 09:20:51 +0200

    more

--- android-project/app/build.gradle.kts
+++ android-project/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation(libs.androidx.games.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
---
+++ android-project/app/src/main/java/io/github/gopher64/gopher64/SlintActivity.java
@@ -0,0 +1,7 @@
+import com.google.androidgamesdk.GameActivity;
+
+public class SlintActivity extends GameActivity {
+    static {
+        System.loadLibrary("gopher64");
+    }
+}
