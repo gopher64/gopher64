6198ad516276a31531552dae5bae096038e94e47
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-23 15:34:34 +0200

    more

--- src/ui/gui.rs
+++ src/ui/gui.rs
@@ -514,13 +514,16 @@ pub fn app_window(app: &AppWindow, is_android: bool) {
     ui::netplay::netplay_window(&app);
     ui::cheats::cheats_window(&app);
 
-    let weak_app = app.as_weak();
-    app.window().on_close_requested(move || {
-        weak_app
-            .upgrade_in_event_loop(move |handle| save_settings(&handle))
-            .unwrap();
-        slint::CloseRequestResponse::HideWindow
-    });
+    #[cfg(not(target_os = "android"))]
+    {
+        let weak_app = app.as_weak();
+        app.window().on_close_requested(move || {
+            weak_app
+                .upgrade_in_event_loop(move |handle| save_settings(&handle))
+                .unwrap();
+            slint::CloseRequestResponse::HideWindow
+        });
+    }
     app.run().unwrap();
     retroachievements::shutdown_client();
 }
