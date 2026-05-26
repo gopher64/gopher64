9d7b13d4c9fe87a2e3dbbf7372e5ce6c2b91e051
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-26 09:39:51 +0200

    include sdl libs for android (#954)

--- android-project/app/src/main/java/io/github/gopher64/gopher64/N64Activity.kt
+++ android-project/app/src/main/java/io/github/gopher64/gopher64/N64Activity.kt
@@ -2,4 +2,11 @@ package io.github.gopher64.gopher64
 
 import org.libsdl.app.SDLActivity
 
-class N64Activity : SDLActivity() {}
+class N64Activity : SDLActivity() {
+    override fun getLibraries(): Array<String> = arrayOf(
+        "SDL3",
+        "SDL3_ttf",
+        "SDL3_image",
+        "gopher64"
+    )
+}
