a447bc810476a4b4cdaded3691da46aad415421a
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-05-26 22:43:21 +0200

    pass all args (#957)

--- android-project/app/src/main/java/io/github/gopher64/gopher64/N64Activity.kt
+++ android-project/app/src/main/java/io/github/gopher64/gopher64/N64Activity.kt
@@ -20,55 +20,18 @@ class N64Activity : SDLActivity() {
 
     override fun getArguments(): Array<String> {
         val intent = intent ?: return super.getArguments()
-        val request_code = intent.getIntExtra("request_code", 0)
-        if (request_code == CONFIGURE_INPUT_PROFILE) {
-            val profile = intent.getStringExtra("profile_name") ?: return super.getArguments()
-            val deadzone = intent.getIntExtra("deadzone", -1)
-            val args = mutableListOf(
-                "--configure-input-profile",
-                profile,
-            )
-            if (intent.getBooleanExtra("dinput", false)) {
-                args.add("--use-dinput")
-            }
-            if (deadzone != -1) {
-                args.add("--deadzone")
-                args.add(deadzone.toString())
-            }
-            setResult(RESULT_OK) // so that the profiles are updated in the GUI
-            return args.toTypedArray()
-        } else if (request_code == RUN_ROM) {
-            val file_path = intent.getStringExtra("file_path") ?: return super.getArguments()
-            val overclock = intent.getBooleanExtra("overclock", false)
-            val disable_expansion_pak = intent.getBooleanExtra("disable_expansion_pak", false)
-            val args = mutableListOf(
-                file_path,
-                "--fullscreen",
-                "--overclock",
-                overclock.toString(),
-                "--disable-expansion-pak",
-                disable_expansion_pak.toString())
+        val args = intent.getStringArrayExtra("args") ?: return super.getArguments()
 
-            val dataIntent = Intent()
-
-            val netplay_peer_addr = intent.getStringExtra("netplay_peer_addr")
-            val cheats = intent.getStringExtra("cheats")
-            if (netplay_peer_addr != null && cheats != null) {
-                args.add("--netplay-peer-addr")
-                args.add(netplay_peer_addr)
-                args.add("--netplay-player-number")
-                args.add(intent.getIntExtra("netplay_player_number", 4).toString())
-                args.add("--cheats")
-                args.add(cheats)
-                dataIntent.putExtra("cheats_path", cheats)
-            }
-
+        val dataIntent = Intent()
+        val file_path = intent.getStringExtra("file_path")
+        if (file_path != null) {
             dataIntent.putExtra("file_path", file_path)
-
-            setResult(RESULT_OK, dataIntent)
-            return args.toTypedArray()
-        } else {
-            return super.getArguments()
         }
+        val cheats_path = intent.getStringExtra("cheats_path")
+        if (cheats_path != null) {
+            dataIntent.putExtra("cheats_path", cheats_path)
+        }
+        setResult(RESULT_OK, dataIntent)
+        return args
     }
 }
