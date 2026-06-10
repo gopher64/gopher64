c4de52b6a8a5d1260e82a1fefd608563023b8577
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-06-10 16:56:30 +0200

    more

--- src/netplay.rs
+++ src/netplay.rs
@@ -299,6 +299,7 @@ fn process_netplay(device: &mut device::Device) {
 
 fn advance_frame(device: &mut device::Device) {
     let netplay = device.netplay.as_mut().unwrap();
+    let local_input = ui::input::get(&mut device.ui, 0, device.speed_limiter.frame_counter);
     let local_handle = *netplay.session.local_player_handles().first().unwrap();
     netplay
         .session
