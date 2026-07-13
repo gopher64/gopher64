282ec6c6c78015d11d4a38aaa35a0942a744949d
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2026-07-13 09:17:35 +0200

    add gpu hints on windows (#1158)

--- parallel-rdp/interface.cpp
+++ parallel-rdp/interface.cpp
@@ -10,6 +10,13 @@
 #include <cmath>
 #include <map>
 
+#ifdef _WIN32
+extern "C" {
+__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
 using namespace Vulkan;
 
 #define DP_STATUS_XBUS_DMA 0x01
